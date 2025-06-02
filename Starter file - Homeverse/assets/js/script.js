aabbc0318149257da2bd01606f6f3b604b51480a
leonardojulius <leonardojulius.jcl@gmail.com>
2025-06-02 03:18:55 +0000

    adding javascript interactions

---
+++ Starter file - Homeverse/assets/js/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+
+const elemToggleFunc = function(elem) {elem.classList.toggle("active");}
+
+
+
+/**
+ * 
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [overlay,navCloseBtn,navOpenBtn];
+
+/**
+ * close navbar when click on any navbar link
+ */
+
+for (let i = 0; i < navbarLinks.length; i ++) {navElemArr.push(navbarLinks[i]);}
+
+/**
+ * event on all elements for togling navbar
+ */
+
+for(let i = 0; i< navElemArr.length; i ++) {
+    navElemArr[i].addEventListener("click", function () {
+        elemToggleFunc(navbar);
+        elemToggleFunc(overlay);
+    });
+}
